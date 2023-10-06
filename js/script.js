efeb08de13cb19fb12e6a48fc145be641c5e8ded
Stugka <pochesnomu@gmail.com>
2023-10-06 17:16:33 +0200

    FirstCode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 "use strict";
+const numberOfFilms = +prompt('How many films have you already watched?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    ganres: [],
+    privat: false
+};
 
-// alert('привет');
+const a = prompt('One of the last movies you watched?', ''),
+      b = prompt('What is your assessment?', ''),
+      c = prompt('One of the last movies you watched?', ''),
+      d = prompt('What is your assessment?', '');
 
-const resoult = confirm("ты дибил?");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
